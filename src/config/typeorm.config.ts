af8b19bef921a25b4f2f0cef637da092f016b5cf
Facundo <facundomoya18@hotmail.com>
2025-09-14 01:02:51 -0300

    se terminaron de relacionar las tablas de la base de datos con migraciones

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,14 +1,14 @@
-import { DataSource } from 'typeorm';
-import { join } from 'path';
+import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
-  type: 'mysql',
-  host: 'localhost',
+  type: "mysql",
+  host: "localhost",
   port: 3306,
-  username: 'root',
-  password: '1234',
-  database: 'schollary',
-  entities: [join(__dirname, '..', '**', '*.entity.{ts,js}')],
-  migrations: [join(__dirname, '..', 'migrations', '*.{ts,js}')],
-  synchronize: true,
+  username: "root",
+  password: "1234",
+  database: "schollary",
+  entities: ["src/**/entities/*.ts"],
+  migrations: ["src/migrations/**/*.ts"],
+  synchronize: false,
+  logging: true,
 });
