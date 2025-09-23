a0688dfc81cc5d73f6c76e16171ddaffc2e30a39
facundo moya <facundomoya18@hotmail.com>
2025-09-23 09:24:21 -0300

    se modifico la importacion de bcrypt

--- src/common/encriptador/bcrypt.ts
+++ src/common/encriptador/bcrypt.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import bcryptjs from 'bcryptjs';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class Bcrypt{
     async encriptar(password: string): Promise<string>{
-        const salt = await bcryptjs.genSalt(10);
-        return await bcryptjs.hash(password, salt);
+        const salt = await bcrypt.genSalt(10);
+        return await bcrypt.hash(password, salt);
     };
 
     async comparar(password: string, hash: string): Promise<boolean>{
-        return await bcryptjs.compare(password, hash);
+        return await bcrypt.compare(password, hash);
     };
 }
